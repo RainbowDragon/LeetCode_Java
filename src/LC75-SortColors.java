6f0f5ee1ef9416185194a0e00336199797b6a821
RainbowDragon <xuanxiang@hotmail.com>
2021-11-24 15:01:31 -0800

    LC75. Sort Colors

--- src/LC75-SortColors.java
+++ src/LC75-SortColors.java
@@ -49,7 +49,7 @@ class Solution {
         }
     }
 
-    public void swap(int[] nums, int first, int second) {
+    void swap(int[] nums, int first, int second) {
 
         int temp = nums[first];
         nums[first] = nums[second];
