1e569d8f6434d1fb03c896ddb9e941d626eabece
RainbowDragon <xuanxiang@hotmail.com>
2021-11-23 18:50:04 -0800

    LC75. Sort Colors

---
+++ src/LC75-SortColors.java
@@ -0,0 +1,58 @@
+/*
+
+LC75. Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Example 3:
+Input: nums = [0]
+Output: [0]
+
+Example 4:
+Input: nums = [1]
+Output: [1]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is 0, 1, or 2.
+
+*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int p0 = 0;
+        int p2 = nums.length - 1;
+
+        for (int p = 0; p <= p2; p++)
+        {
+            if (nums[p] == 0) {
+                swap(nums, p0, p);
+                p0++;
+            }
+            else if (nums[p] == 2) {
+                swap(nums, p2, p);
+                p2--;
+                p--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int first, int second) {
+
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
