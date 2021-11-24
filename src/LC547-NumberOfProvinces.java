797e3c66f5084095b26caaecac3adafa3da68c8f
RainbowDragon <xuanxiang@hotmail.com>
2021-11-24 15:25:03 -0800

    LC547. Number of Provinces

---
+++ src/LC547-NumberOfProvinces.java
@@ -0,0 +1,61 @@
+/*
+
+LC547. Number of Provinces
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+Return the total number of provinces.
+
+Example 1:
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+
+Example 2:
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+
+Constraints:
+1 <= n <= 200
+n == isConnected.length
+n == isConnected[i].length
+isConnected[i][j] is 1 or 0.
+isConnected[i][i] == 1
+isConnected[i][j] == isConnected[j][i]
+
+*/
+
+class Solution {
+    public int findCircleNum(int[][] M) {
+
+        if (M == null || M.length == 0) {
+            return 0;
+        }
+
+        int n = M.length;
+        boolean[] visited = new boolean[n];
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i]) {
+                result++;
+                dfs(M, visited, i);
+            }
+        }
+
+        return result;
+    }
+
+    void dfs(int[][] M, boolean[] visited, int i)
+    {
+        visited[i] = true;
+
+        for (int j = 0; j < M.length; j++)
+        {
+            if (M[i][j] == 1 && !visited[j]) {
+                dfs(M, visited, j);
+            }
+        }
+    }
+}
