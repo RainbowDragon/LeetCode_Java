82a9367f99333c16fc42ce0e05f60eec955f786e
RainbowDragon <xuanxiang@hotmail.com>
2021-11-23 16:14:25 -0800

    LC198. House Robber

--- src/LC198-HouseRobber.java
+++ src/LC198-HouseRobber.java
@@ -18,7 +18,6 @@ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (m
 Total amount you can rob = 2 + 9 + 1 = 12.
 
 Constraints:
-
 1 <= nums.length <= 100
 0 <= nums[i] <= 400
 
