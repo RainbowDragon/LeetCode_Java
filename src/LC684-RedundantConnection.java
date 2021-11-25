4d6195c9e1165923c87f66c307bcbab57fae2113
RainbowDragon <xuanxiang@hotmail.com>
2021-11-24 16:26:49 -0800

    LC684. Redundant Connection

---
+++ src/LC684-RedundantConnection.java
@@ -0,0 +1,84 @@
+/*
+
+LC684. Redundant Connection
+
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+
+Example 1:
+Input: edges = [[1,2],[1,3],[2,3]]
+Output: [2,3]
+
+Example 2:
+Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+Output: [1,4]
+
+Constraints:
+n == edges.length
+3 <= n <= 1000
+edges[i].length == 2
+1 <= ai < bi <= edges.length
+ai != bi
+There are no repeated edges.
+The given graph is connected.
+
+*/
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+
+        if (edges == null || edges.length == 0) {
+            return null;
+        }
+
+        DSU dsu = new DSU(edges.length+1);
+        for (int[] edge : edges)
+        {
+            if (!dsu.union(edge[0], edge[1])) {
+                return edge;
+            }
+        }
+
+        return null;
+    }
+
+    class DSU {
+
+        int[] parent;
+
+        public DSU(int size) {
+
+            parent = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+
+            if (parent[x] != x) {
+                int p = find(parent[x]);
+                parent[x] = p;
+            }
+
+            return parent[x];
+        }
+
+        public boolean union(int x, int y) {
+
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return false;
+            }
+            else {
+                parent[rootX] = parent[rootY] = Math.min(rootX, rootY);
+                return true;
+            }
+        }
+    }
+}
+
